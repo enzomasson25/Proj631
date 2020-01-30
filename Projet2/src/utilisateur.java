0394f7066f2eb016f425ee0d0f231ef3a8dbbc43
enzo masson <39915492+enzomasson25@users.noreply.github.com>
2020-01-30 16:55:38 +0100

    Update utilisateur.java

--- Projet2/src/utilisateur.java
+++ Projet2/src/utilisateur.java
@@ -1,7 +1,35 @@
+import java.util.List;
+
 public class utilisateur {
 
 	private int id;
-	private List<int> liste_donnee_interet;
+	private List<Integer> liste_donnee_interet;
+	private int id_noeud;
 	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public List<Integer> getListe_donnee_interet() {
+		return liste_donnee_interet;
+	}
+	public void setListe_donnee_interet(List<Integer> liste_donnee_interet) {
+		this.liste_donnee_interet = liste_donnee_interet;
+	}
+	public int getId_noeud() {
+		return id_noeud;
+	}
+	public void setId_noeud(int id_noeud) {
+		this.id_noeud = id_noeud;
+	}
+	
+	public utilisateur(int id, List<Integer> liste_donnee_interet, int id_noeud) {
+		super();
+		this.id = id;
+		this.liste_donnee_interet = liste_donnee_interet;
+		this.id_noeud = id_noeud;
+	}
 	
 }
