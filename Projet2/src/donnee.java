ded76c28fbc53cd815c7263795a7edd08d404d08
enzo masson <39915492+enzomasson25@users.noreply.github.com>
2020-01-30 16:30:43 +0100

    Update donnee.java

--- Projet2/src/donnee.java
+++ Projet2/src/donnee.java
@@ -8,6 +8,28 @@
  */
 public class donnee {
 
+	private int id;
+	private int taille; 
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getTaille() {
+		return taille;
+	}
+	public void setTaille(int taille) {
+		this.taille = taille;
+	}
+	
+	public donnee(int id, int taille) {
+		super();
+		this.id = id;
+		this.taille = taille;
+	}
+	
 	
 
 }
